575733739cd59715ad24ea3b0f6760e5082d9a8a
AyushJ16 <ayush.jaiswal.che21@iitbhu.ac.in>
2023-12-03 12:27:39 +0000

    initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Example using for...in loop
+const obj = { a: 1, b: 2, c: 3 };
+
+for (let key in obj) {
+  console.log(key, obj[key]);
+}
+
+// Example using for...of loop
+const arr = [1, 2, 3, 4, 5];
+
+for (let element of arr) {
+  console.log(element);
+}
