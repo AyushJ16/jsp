8f444c822e9ca2fe7e31dc208cde2eaa152dbc50
AyushJ16 <ayush.jaiswal.che21@iitbhu.ac.in>
2023-12-07 09:35:43 +0000

    asynchronous done

--- index.js
+++ index.js
@@ -1,82 +1,141 @@
-
-// Example using for...in loop
-const obj = { a: 1, b: 2, c: 3 };
-
-for (let key in obj) {
-  console.log(key, obj[key]);
-}
-
-// Example using for...of loop
-const arr = [1, 2, 3, 4, 5];
-
-for (let element of arr) {
-  console.log(element);
-}
-//example of forEach vs for in
-const array = ['a', 'b', 'c'];
-array.forEach((item) => {
-  console.log(`${item}`);
-  });
-  // a
-  // b
-  // c
-  let sum=0;
-  arr.forEach((num)=>{sum+=num});
-  console.log('Summation with For Each:'+sum);
- const ob=[1,2,3]
- ob.forEach((num)=>console.log(num));
-
- //forEach to iterate over objects
- const person={name:"John",age:30};
- Object.keys(person).forEach((key)=>console.log(`Key : ${key}, Value : ${person[key]}`))
-
-    // Key a: Value 1
-    // Key b: Value 2
-    // Key c: Value 3
-//     //example of forEach vs for of
 let divs=document.querySelectorAll("div");
 
 
-console.log(divs);
-divs.forEach((div)=>{
-    div.style.color="red";
-});
-// for(let div in divs){
-//     div.style.color="blue"; 
-// }
-//for in
-for(let div of divs){
-    div.style.color="blue";
-}
-let button=document.querySelector("button");
-// button.addEventListener("click",()=>{
-//   if(document.body.style.backgroundColor!=="black")  
-//   document.body.style.backgroundColor="black";
-//   else{
-//     document.body.style.backgroundColor="white";
-//   }
-//   });
-button.onclick=(evt)=>{
-  if(document.body.style.backgroundColor!=='black'){
-    document.body.style.backgroundColor='black';}
-  
-  else{
-    document.body.style.backgroundColor='white';
-  }  
-}
-//understood JSON web tokens, cookies
-//NodeJs is a runtime environment not a framework whereas Express is a framework to write server side applications
-//by express we are creating server for the website
 
-//callback relation
-let order =(call_production)=>{
-  console.log("Order placed. Please call production");
-  call_production();
+// console.log(divs);
+// divs.forEach((div)=>{
+//     div.style.color="red";
+// });
+// // for(let div in divs){
+// //     div.style.color="blue"; 
+// // }
+// //for in
+// for(let div of divs){
+//     div.style.color="blue";
+// }
+// let button=document.querySelector("button");
+// // button.addEventListener("click",()=>{
+// //   if(document.body.style.backgroundColor!=="black")  
+// //   document.body.style.backgroundColor="black";
+// //   else{
+// //     document.body.style.backgroundColor="white";
+// //   }
+// //   });
+// button.onclick=(evt)=>{
+//   if(document.body.style.backgroundColor!=='black'){
+//     document.body.style.backgroundColor='black';}
+  
+//   else{
+//     document.body.style.backgroundColor='white';
+//   }  
+// }
+let stocks={
+  Fruits : ["Apple", "Orange", "Ban", "strawberry"],
+  liquid: ["water","ice"],
+  holder: ["cone","cup","stick"],
+  toppings : ["chocolate","peanuts"],
+
 }
-let production=()=>{
-  console.log("order placed. starting production");
+// let order =(fruit_name, call_production)=>{
+//   setTimeout(() => {
+//     console.log(`${stocks.Fruits[fruit_name]}`);
+//     call_production();
+//   }, 2000);
+//   console.log("Order placed. Please call production");
+// }
+// let production=()=>{
+//   setTimeout(()=>{
+//     console.log(`Production has started`);
+//     setTimeout(() => {
+//       console.log(`Fruit has been chopped`);
+//       setTimeout(() => {
+//         console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+//         setTimeout(()=>{
+//           console.log(`${stocks.holder[0]} and ${stocks.holder[1]} were used`);
+//           setTimeout(()=>{
+//             console.log(`Topping ${stocks.toppings[0]} and ${stocks.toppings[1]} were added.`);
+//             setTimeout(()=>{
+//               console.log('The icecream is ready');
+//             },1000);
+//             },3000)  
+//         },2000);
+//       }, 1000);
+//     }, 2000);
+//   },0000);
+//   console.log("order placed. starting production");
+// }
+// order(0,production);
+
+//promises
+// let is_shop_open=true;
+// let order=(time,work)=>{
+//   return new Promise((resolve,reject)=>{
+//     if (is_shop_open){
+//       setTimeout(() => {
+//         resolve(work());
+//       }, time);
+//       }else{
+//         reject(`shop is closed`);
+//       };
+
+//   });
+// }
+// order(2000,()=>{
+//   console.log(`${stocks.Fruits[0]} was selected`);
+// })
+// .then(()=>{
+//   return order(0000,()=>{
+//     console.log(`Production has started`);
+//   });
+// })
+// .catch(()=>{
+//   console.log("Shop is closed");
+// })
+// .finally(()=>{
+//   console.log("Order completed");
+// })
+//async/await
+let is_shop_open=false;
+function time(ms){
+  return new Promise((resolve, reject) =>{
+    if(is_shop_open){
+      setTimeout(resolve,ms);
+    }
+    else{
+      reject(console.log("shop is closed"));
+    }
+  })
 }
-order(production);
+async function kitchen(){
+
+  console.log(`dekhte hai`)
+  try{
+    await time(2000);
+    console.log(`${stocks.Fruits[0]} was selected`);
+    await time(0000);
+    console.log(`Production has started`);
+    await time(2000);
+    console.log(`Fruit has been chopped`);
+    await time(1000);
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+    await time(1000);
+    console.log(`${stocks.holder[0]} and ${stocks.holder[1]} were used`);
+    await time(3000);
+    console.log(`Topping ${stocks.toppings[0]} and ${stocks.toppings[1]} were added.`);
+    await time(2000);
+    console.log('The icecream is ready');
+  }
+  catch(error){
+    console.log("customer left",error);
+    // throw error;
+  }
+  finally{
+    console.log("Day ended, shop is closed");
+  }
+}
+kitchen();
+          
+
 
 
 
