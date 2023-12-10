dcbae6445c550102f96b91efd582fc96d91082c0
AyushJ16 <ayush.jaiswal.che21@itbhu.ac.in>
2023-12-10 15:52:16 +0530

    cookies updated

--- index.js
+++ index.js
@@ -95,47 +95,51 @@ let stocks={
 //   console.log("Order completed");
 // })
 //async/await
-let is_shop_open=true;
-function time(ms){
-  return new Promise((resolve, reject) =>{
-    if(is_shop_open){
-      setTimeout(()=>{
-        resolve("ok");
-      },ms);
-    }
-    else{
-      reject(("shop is closed"));
-    }
-  })
-}
-async function kitchen(){
+// let is_shop_open=true;
+// function time(ms){
+//   return new Promise((resolve, reject) =>{
+//     if(is_shop_open){
+//       setTimeout(()=>{
+//         resolve("ok");
+//       },ms);
+//     }
+//     else{
+//       reject(("shop is closed"));
+//     }
+//   })
+// }
+// async function kitchen(){
 
-  console.log(`dekhte hai`)
-  try{
-    await time(2000);
-    console.log(`${stocks.Fruits[0]} was selected`);
-    await time(1000);
-    console.log(`Production has started`);
-    await time(2000);
-    console.log(`Fruit has been chopped`);
-    await time(1000);
-    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-    await time(1000);
-    console.log(`${stocks.holder[0]} and ${stocks.holder[1]} were used`);
-    await time(3000);
-    console.log(`Topping ${stocks.toppings[0]} and ${stocks.toppings[1]} were added.`);
-    await time(2000);
-    console.log('The icecream is ready');
-  }
-  catch(error){
-    console.log("customer left",error);
-    // throw error;
-  }
-  finally{
-    console.log("Day ended, shop is closed");
-  }
-}
-kitchen();
+//   console.log(`dekhte hai`)
+//   try{
+//     await time(2000);
+//     console.log(`${stocks.Fruits[0]} was selected`);
+//     await time(1000);
+//     console.log(`Production has started`);
+//     await time(2000);
+//     console.log(`Fruit has been chopped`);
+//     await time(1000);
+//     console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+//     await time(1000);
+//     console.log(`${stocks.holder[0]} and ${stocks.holder[1]} were used`);
+//     await time(3000);
+//     console.log(`Topping ${stocks.toppings[0]} and ${stocks.toppings[1]} were added.`);
+//     await time(2000);
+//     console.log('The icecream is ready');
+//   }
+//   catch(error){
+//     console.log("customer left",error);
+//     // throw error;
+//   }
+//   finally{
+//     console.log("Day ended, shop is closed");
+//   }
+// }
+// kitchen();
+console.log(document.cookie);
+document.cookie="name=shubham";
+console.log(document.cookie);
+
           
 
 
